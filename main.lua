9095e4379c94fd7bc344a2015a54589996945311
AkkermanFunc <AkkermanFunc@github.com>
2019-04-12 17:34:08 +0500

    Everything done

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local composer = require( "composer" )
+Object = require("lib.classic.classic")
 
 display.setStatusBar( display.HiddenStatusBar )
 
 math.randomseed( os.time() )
 
-composer.gotoScene("intro")
+composer.gotoScene("scene.intro")
